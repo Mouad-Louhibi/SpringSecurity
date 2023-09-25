e99dfbdaff97936442d791d1f70e74755e705689
Mouad <mouadlouhibi2000@gmail.com>
2023-09-25 01:42:25 +0100

    SECRET_KEY

--- src/main/java/com/louhibi/security/config/JwtService.java
+++ src/main/java/com/louhibi/security/config/JwtService.java
@@ -1,10 +1,32 @@
 package com.louhibi.security.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
+
+    private static final String SECRET_KEY = "49BAAAB739B9CBC32F9892BAB13C7";
     public String extractUsername(String token) {
         return null;
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSingInKey())
+                .build()
+                .parseClaimsJwt(token)
+                .getBody();
+    }
+
+    private Key getSingInKey() {
+        byte [] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
